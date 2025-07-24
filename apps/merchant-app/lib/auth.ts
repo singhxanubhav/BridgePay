1e3fb44e36383c355ff6d485a9cd53bd067ca2f1
Anubhav Singh <anubhavsinghbkj@gmail.com>
2025-07-24 07:00:06 +0530

    some changes

--- apps/merchant-app/lib/auth.ts
+++ apps/merchant-app/lib/auth.ts
@@ -1,48 +1,91 @@
-import GoogleProvider from "next-auth/providers/google";
 import db from "@repo/db/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { z } from "zod";
 
+const credentialsSchema = z.object({
+  phone: z.string().min(6).max(12),
+  password: z.string(),
+});
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        })
-    ],
-    callbacks: {
-      async signIn({ user, account }: {
-        user: {
-          email: string;
-          name: string
+  providers: [
+    CredentialsProvider({
+      name: "Phone",
+      credentials: {
+        phone: {
+          label: "Phone number",
+          type: "text",
+          placeholder: "1231231231",
+          required: true,
         },
-        account: {
-          provider: "google" | "github"
+        password: { label: "Password", type: "password", required: true },
+      },
+
+      async authorize(credentials?: { phone: string; password: string }) {
+        if (!credentials || !credentials.phone || !credentials.password) {
+          console.error("Missing credentials");
+          return null;
         }
-      }) {
-        console.log("hi signin")
-        if (!user || !user.email) {
-          return false;
+   
+        const result = credentialsSchema.safeParse(credentials);
+        if (!result.success) {
+          console.error(result.error);
+          return null;
         }
 
-        await db.merchant.upsert({
-          select: {
-            id: true
-          },
+        const hashedPassword = await bcrypt.hash(credentials.password, 10);
+        const existingUser = await db.user.findFirst({
           where: {
-            email: user.email
+            number: credentials.phone,
           },
-          create: {
-            email: user.email,
-            name: user.name,
-            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
-          },
-          update: {
-            name: user.name,
-            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
-          }
         });
 
-        return true;
-      }
+        if (existingUser) {
+          const passwordValidation = await bcrypt.compare(
+            credentials.password,
+            existingUser.password,
+          );
+          if (passwordValidation) {
+            return {
+              id: existingUser.id.toString(),
+              name: existingUser.name,
+              email: existingUser.number,
+            };
+          }
+          return null;
+        }
+
+        try {
+          const user = await db.user.create({
+            data: {
+              number: credentials.phone,
+              password: hashedPassword,
+            },
+          });
+
+          return {
+            id: user.id.toString(),
+            name: user.name,
+            email: user.number,
+          };
+        } catch (e) {
+          console.error(e);
+        }
+
+        return null;
+      },
+    }),
+  ],
+  secret: process.env.JWT_SECRET || "secret",
+  callbacks: {
+    // TODO: can u fix the type here? Using any is bad
+    async session({ session, token }: any) {
+      // Assuming `sub` is the user ID stored in the token
+      session.user.id = token.sub;
+      return session;
     },
-    secret: process.env.NEXTAUTH_SECRET || "secret"
-  }
+  },
+  pages: {
+    signIn: "/auth",
+  },
+};
